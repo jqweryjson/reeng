b3909d78d70d60c32c671b5f561f91f93fde0189
kirill <jqweryjson@gmail.com>
2020-09-03 12:57:18 +0300

    #1 Init Project, add Pug and Stilus loaders

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+#app
+  .test
+    | HelloWorld
+
 </template>
 
 <script lang="ts">
@@ -17,13 +17,8 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+#app
+  color red
+
 </style>
